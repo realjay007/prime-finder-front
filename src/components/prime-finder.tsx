48edb99d6cffbe426551f551d6d355ef7b6d0cfa
Julius Ijie <juliusijie@gmail.com>
2021-05-25 09:00:13 +0100

    Implement loader feature

--- src/components/prime-finder.tsx
+++ src/components/prime-finder.tsx
@@ -1,18 +1,33 @@
 import React, { ChangeEvent, Component } from 'react';
+import Loader from 'react-spinners/PulseLoader';
 import { findLowerPrime } from '../lib/api';
 import "./prime-finder.css";
 
 type State = {
 	input?: number,
 	result?: number,
+	loading: boolean,
 }
 
 export default class PrimeFinder extends Component<{}, State> {
-	state: State = {}
+	state: State = {
+		loading: false,
+	}
 
 	private onInputChange = (ev: ChangeEvent<HTMLInputElement>) => {
 		const input = Number(ev.target.value);
 		this.setState({ input });
+	}
+
+	private onSubmit = async () => {
+		const { input = null, loading } = this.state;
+		if (loading) return;
+		if (typeof input !== 'number') return alert('Please enter a number');
+
+		this.setState({ loading: true });
+		const response = await findLowerPrime(input);
+		const result: number = response.data;
+		this.setState({ result, loading: false });
 	}
 
 	private renderResult() {
@@ -21,21 +36,16 @@ export default class PrimeFinder extends Component<{}, State> {
 		return <h1 className="inactive">RESULT</h1>
 	}
 
-	private onSubmit = async () => {
-		const { input = null } = this.state;
-		if (typeof input !== 'number') return alert('Please enter a number');
-
-		const response = await findLowerPrime(input);
-		const result: number = response.data;
-		this.setState({ result });
-	}
-
 	public render() {
+		const { loading } = this.state;
+
 		return (
 			<div className="App">
 				<header className="App-header">
 					<input type="number" placeholder="Enter number" className="input" min="3" onChange={this.onInputChange} />
-					<button onClick={this.onSubmit}>Submit</button>
+					<button onClick={this.onSubmit}>
+						{loading ? (<Loader color="#fff" />) : 'Submit'}
+					</button>
 					{this.renderResult()}
 				</header>
 			</div>
